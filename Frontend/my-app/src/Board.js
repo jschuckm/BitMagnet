712bcbc6cb67003727ed7053dc85c2b89afdb61a
epsimp <epsimp@iastate.edu>
2021-03-29 11:05:21 -0500

    taking title and content in board page

--- Frontend/my-app/src/Board.js
+++ Frontend/my-app/src/Board.js
@@ -10,54 +10,80 @@ import './App.css';
 class Board extends React.Component {
     constructor(props) {
         super(props);
-        this.handleOpenDialog=this.handleOpenDialog.bind(this);
+        this.handleOpenNewDialog=this.handleOpenNewDialog.bind(this);
         this.handleCloseDialog=this.handleCloseDialog.bind(this);
         this.handleOpenDialogDelete=this.handleOpenDialogDelete.bind(this);
+
+        this.handleOpenTextDialog=this.handleOpenTextDialog(this);
+        this.handleCloseTextDialog=this.handleCloseTextDialog(this);
+
         this.handleCloseDialogDelete=this.handleCloseDialogDelete.bind(this);
         this.createMagnet=this.createMagnet.bind(this);
+
+        this.createMagnetText=this.createMagnetText.bind(this);
+
         this.deleteMagnet=this.deleteMagnet.bind(this);
         this.printMagnets=this.printMagnets.bind(this);
         this.state = {
-            magnets: []
+            magnets: [] //will have title and content
         };
     }
 
-    handleOpenDialog(){
-      this.setState({openDialog: true});
+    handleOpenNewDialog(){
+      this.setState({openNewDialog: true});
     }
 
     handleCloseDialog(){
-      this.setState({openDialog: false});
+      this.setState({openNewDialog: false});
     }
 
     handleOpenDialogDelete(){
         this.setState({openDialogDelete: true});
     }
+
+    handleOpenTextDialog() {
+      this.setState({openTextDialog: true});
+    }
+
+    handleCloseTextDialog() {
+      this.setState({openTextDialog:false});
+    }
   
     handleCloseDialogDelete(){
         this.setState({openDialogDelete: false});
     }
+
     createMagnet(){
-        console.log(this.state.magnets);
-        console.log(this.state.newMagnet);
-        this.state.magnets.push({text:this.state.newMagnet});
-        console.log(this.state.magnets);
-        this.setState({openDialog:false});
+        //console.log(this.state.magnets);
+        //console.log(this.state.newMagnet);
+        //this.state.magnets.push({title:this.state.newMagnetTitle, content: ''}); **PUSH OCCURS AFTER CONTENT IS OBTAINED
+        //console.log(this.state.magnets);
+        this.setState({openNewDialog:false});
+        this.setState({openTextDialog:true});
     }
+
+    createMagnetText() {
+      console.log(this.state.magnets);
+      console.log(this.state.newMagnetText);
+      this.state.magnets.push({title:this.state.newMagnetTitle, content: this.state.newMagnetText});
+      console.log(this.state.magnets);
+      this.setState({openTextDialog:false});
+    }
+
     deleteMagnet(){
         this.setState({openDialogDelete:false});
         for( let i = 0;i<this.state.magnets.length;i++){
-            if(this.state.magnets[i].text==this.state.deleteMagnet){
+            if(this.state.magnets[i].title==this.state.deleteMagnet){
                 this.state.magnets.splice(i,1);
             }
         }
     }
     printMagnets() {
         return this.state.magnets.map((magnet) => {
-            console.log(magnet.text);
+            console.log(magnet.title);
             return (
               <Typography variant='h6' style={{fontFamily: 'Monospace'}}>
-              <p>{magnet.text}</p>
+              <p>{magnet.title}:{magnet.content}</p>
               </Typography>
             )
           });
@@ -83,16 +109,31 @@ class Board extends React.Component {
               borderLeft: '1px solid black',
               borderRight: '1px solid black'
             }}>
-              <Dialog open={this.state.openDialog} onClose={this.handleCloseDialog}>
+              <Dialog open={this.state.openNewDialog} onClose={this.handleCloseDialog}>
                 <DialogTitle data-testid="createPopup">Add Magnet</DialogTitle>
                 <DialogContent>
-                  <TextField data-testid="createMagTxt"onChange={(event) => this.setState({newMagnet: event.target.value})} label={"Magnet Text"}/><br></br>
+                  <TextField data-testid="createMagTxt"onChange={(event) => this.setState({newMagnetTitle: event.target.value})} label={"Magnet Title"}/><br></br>
                 </DialogContent>
                 <DialogActions>
                   <Button data-testid="createMagSubmitBtn" onClick={this.createMagnet} style={{marginRight: '55px'}}>
                     Create Magnet
                   </Button>
                   <Button data-testid="closeCreatePopup" onClick={this.handleCloseDialog}>
+                    Back
+                  </Button>
+                </DialogActions>
+              </Dialog>
+
+              <Dialog open={this.state.openTextDialog} onClose={this.handleCloseDialog}>
+                <DialogTitle data-testid="createTextPopup">Enter Text</DialogTitle>
+                <DialogContent>
+                  <TextField data-testid="createMagTxtContent"onChange={(event) => this.setState({newMagnetText: event.target.value})} label={"Magnet Text"}/><br></br>
+                </DialogContent>
+                <DialogActions>
+                  <Button data-testid="createMagTextSubmitBtn" onClick={this.createMagnetText} style={{marginRight: '55px'}}>
+                    Post Text
+                  </Button>
+                  <Button data-testid="closeCreateTextPopup" onClick={this.handleCloseTextDialog}>
                     Back
                   </Button>
                 </DialogActions>
@@ -128,12 +169,12 @@ class Board extends React.Component {
                 borderTop: '1px solid black',
                 borderBottom: '1px solid black'
               }}>
-                  <Typography variant='h5' style={{marginLeft: '20px', marginTop: '20px', fontFamily: 'Monospace', display: 'block'}}>
+                  <span style={{marginLeft: '20px', marginTop: '20px', fontFamily: 'Monospace', display: 'block'}}>
                     {this.printMagnets()}
-                  </Typography>
+                  </span>
               </div>
 
-              <Button data-testid="addMagnetBtn" style={{marginLeft: '265px', marginTop: '10px'}} onClick={this.handleOpenDialog}>
+              <Button data-testid="addMagnetBtn" style={{marginLeft: '265px', marginTop: '10px'}} onClick={this.handleOpenNewDialog}>
                 Add magnet
               </Button>
               <Button data-testid="deleteMagnetBtn" style={{marginLeft: '265px', marginTop: '10px'}} onClick={this.handleOpenDialogDelete}>
